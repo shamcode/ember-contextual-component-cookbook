5ba18334f8eaaddaa5c1db5386fc4e3b629171fa
Eugeny Burnashov <shamcode@gmail.com>
2017-09-30 15:49:25 +0700

    Rebuild

--- app/routes/what-is-it.js
+++
@@ -1,9 +0,0 @@
-import Route from "@ember/routing/route";
-
-export default Route.extend({
-  model(){
-    return {
-      contextualComponentLink: 'https://guides.emberjs.com/v2.14.0/components/wrapping-content-in-a-component/#toc_sharing-component-data-with-its-wrapped-content'
-    }
-  }
-});
